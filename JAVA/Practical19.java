a6c175dde24c7b21dedd7ae9ccc0a622bcbc68ef
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-17 23:48:57 +0530

    Update Practical19.java

--- JAVA/Practical19.java
+++ JAVA/Practical19.java
@@ -1,18 +1,41 @@
+class Vehicle {
+    private String make;
+    private int year;
+    private String color;
+    private String model;
+
+    public Vehicle(String make, int year, String color, String model) {
+        this.make = make;
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+
+    public void getInfo() {
+        System.out.println("Manufactured by: " + make);
+        System.out.println("Year: " + year);
+        System.out.println("Color: " + color);
+        System.out.println("Model: " + model);
+    }
+}
+
+class Car extends Vehicle {
+    private String bodyStyle;
+
+    public Car(String make, int year, String color, String model, String bodyStyle) {
+        super(make, year, color, model);
+        this.bodyStyle = bodyStyle;
+    }
+
+    public void getCarInfo() {
+        getInfo();
+        System.out.println("Body Style: " + bodyStyle);
+    }
+}
+
 public class Practical19 {
     public static void main(String[] args) {
-        D obj = new D();
-        obj.print();
-    }
+        Car Elantra = new Car("Hyundai", 2019, "Blue", "Elantra", "Sedan");
+        Elantra.getCarInfo();
+    }    
 }
-
-class A {
-    public void print() {
-        System.out.println("This is Multilevel Inheritance");
-    }
-}
-
-class B extends A {}
-
-class C extends B {}
-
-class D extends C {}

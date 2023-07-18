c3980864376a62000c0a328d55aa490bee530438
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-18 16:23:31 +0530

    Update Practical18.java

--- JAVA/Practical18.java
+++ JAVA/Practical18.java
@@ -1,24 +1,18 @@
 public class Practical18 {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();
+        Car car = new Car();
+        car.drive();  
     }
 }
 
-class Animal {
-    void eat() {
-        System.out.println("The animal is eating.");
+class Vehicle {
+    void drive() {
+        System.out.println("Driving a vehicle.");
     }
 }
 
-class Mammal extends Animal {
-    void walk() {
-        System.out.println("The mammal is walking.");
-    }
-}
-
-class Dog extends Mammal {
-    void bark() {
-        System.out.println("The dog is barking.");
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Starting the car.");
     }
 }

1449f8502311cab429fd1cb988b1fc18047ea155
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-22 22:11:10 +0530

    Add files via upload

---
+++ JAVA/Practical24.java
@@ -0,0 +1,8 @@
+public class Practical24 {
+    public static void main(String[] args) {
+        // Init of Array
+        int A[] = {1, 2, 3, 4, 5};
+        for (int i = 0; i < A.length; ++i)
+            System.out.println(A[i]);
+    }
+}

4c6bd42fcdc4d784ea210c4138bdd16e5d3f23a5
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-25 16:25:12 +0530

    Update Practical3.java

--- JAVA/Practical3.java
+++ JAVA/Practical3.java
@@ -5,9 +5,9 @@ public class Practical3 {
         // Simple Interest  = P * R * N / 100
         Scanner cin = new Scanner(System.in);
         double P, R, N;
-        P = cin.nextInt();
-        R = cin.nextInt();
-        N = cin.nextInt();
+        P = cin.nextDouble();
+        R = cin.nextDouble();
+        N = cin.nextDouble();
 
         System.out.println("SI = " + P * R * N * 0.01); // 1 / 100 = 0.01
     }

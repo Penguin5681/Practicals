8d72721f8e28014000e5958b84d206239bf0e399
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-29 17:23:24 +0530

    Update

--- JAVA/Practical15.java
+++ JAVA/Practical15.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class Practical15 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+
+        System.out.println("Choose operation: ");
         char ch = cin.next().charAt(0);
+        
             switch (ch) {
-                case '+': System.out.println("This is +"); break;
-                case '-': System.out.println("This is -"); break;
-                case '/': System.out.println("This is /"); break;
-                default: System.out.println("Enter a valid case: + - /");
+                case '+': System.out.println(a + b); break;
+                case '-': System.out.println(a - b); break;
+                case '/': System.out.println(a / b); break;
+                case '*': System.out.println(a * b);
+                default: System.out.println("Enter a valid case: + - / *");
             }
     }
 }

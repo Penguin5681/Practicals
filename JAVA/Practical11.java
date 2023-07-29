1bc995e570a761e7e45235fde47a1186851b49d7
Pranav Sinha <pranavsinha922@gmail.com>
2023-07-29 15:33:29 +0530

    update

--- JAVA/Practical11.java
+++ JAVA/Practical11.java
@@ -4,6 +4,7 @@ public class Practical11 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         float temperature = cin.nextFloat();
+        
         if (temperature < 30.0 && temperature > 0)
             System.out.println("Nice Weather");
         else if (temperature > 30 && temperature < 50)

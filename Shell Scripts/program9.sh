b5f3a1478492580b7e0964accbd137efe70484b7
Pranav Sinha <pranavsinha922@gmail.com>
2023-09-05 13:45:23 +0530

    Create program9.sh

---
+++ Shell Scripts/program9.sh
@@ -0,0 +1,36 @@
+
+
+echo "Simple Calculator"
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+echo "Select operation:"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read choice
+
+case $choice in
+    1)
+        result=$((num1 + num2))
+        ;;
+    2)
+        result=$((num1 - num2))
+        ;;
+    3)
+        result=$((num1 * num2))
+        ;;
+    4)
+        result=$(awk "BEGIN {printf \"%.2f\", $num1 / $num2}")
+        ;;
+    *)
+        echo "Invalid choice"
+        exit 1
+        ;;
+esac
+
+echo "Result: $result"

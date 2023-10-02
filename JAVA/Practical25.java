d36313e226c1056edb8f73384612116f130b8a76
Pranav Sinha <pranavsinha922@gmail.com>
2023-10-02 23:20:53 +0530

    Create Practical25.java

---
+++ JAVA/Practical25.java
@@ -0,0 +1,17 @@
+public class Practical25 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Display the elements of the multidimensional array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

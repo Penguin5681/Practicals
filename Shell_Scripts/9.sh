f7ef497cf50b386c1108e152ce51f2afc8717239
Pranav Sinha <pranavsinha922@gmail.com>
2023-10-25 22:15:12 +0530

    Update 9.sh

--- Shell_Scripts/9.sh
+++ Shell_Scripts/9.sh
@@ -1,44 +1,79 @@
 #!/bin/bash
 
-db_file="library_db.txt"
-
 while true; do
-    clear
-    echo "Library Management System"
-    echo "a. VIEW RECORD BASED ON QUERY"
-    echo "b. ADD RECORD"
-    echo "c. DELETE RECORD"
-    echo "d. COUNT TOTAL NUMBER OF RECORDS"
-    echo "e. EXIT"
+    echo "Enter your choice:"
+    echo "1. Insert"
+    echo "2. View"
+    echo "3. Count"
+    echo "4. Search"
+    echo "5. Delete"
+    echo "6. Update"
+    echo "7. Exit"
+    read choice
 
-    read -p "Enter your choice (a/b/c/d/e): " choice
+    case $choice in
+    1)
+        echo "Enter account number:"
+        read number
+        echo "Enter title:"
+        read title
+        echo "Enter author:"
+        read author
+        echo "Enter edition:"
+        read edition
+        echo "Enter publisher:"
+        read publisher
 
-    case "$choice" in
-        a)
-            read -p "Enter a query: " query
-            grep -i "$query" "$db_file"
-            ;;
-        b)
-            read -p "Enter record (Account Number Title Author Edition Publisher): " record
-            echo "$record" >> "$db_file"
-            echo "Record added successfully."
-            ;;
-        c)
-            read -p "Enter Account Number to delete: " acc_num
-            grep -v "$acc_num" "$db_file" > tmpfile && mv tmpfile "$db_file"
-            echo "Record deleted successfully."
-            ;;
-        d)
-            echo "Total number of records: $(wc -l < "$db_file")"
-            ;;
-        e)
-            echo "Goodbye!"
-            exit 0
-            ;;
-        *)
-            echo "Invalid choice. Please try again."
-            ;;
+        echo "$number|$title|$author|$edition|$publisher" >> record_data.txt
+        echo "Data inserted successfully"
+        ;;
+
+    2)
+        cat record_data.txt
+        ;;
+
+    3)
+        count=$(wc -l < record_data.txt)
+        echo "Total number of records: $count"
+        ;;
+
+    4)
+        echo "Enter title to search:"
+        read title
+        grep -i "$title" record_data.txt
+        ;;
+
+    5)
+        echo "Enter account number to delete:"
+        read number
+        grep -v "$number" record_data.txt > record_data1.txt
+        mv record_data1.txt record_data.txt
+        echo "Record deleted successfully"
+        ;;
+
+    6)
+        echo "Enter account number to update:"
+        read number
+        echo "Enter title:"
+        read title
+        echo "Enter author:"
+        read author
+        echo "Enter edition:"
+        read edition
+        echo "Enter publisher:"
+        read publisher
+
+        sed -i "s/^$number|.*$/$number|$title|$author|$edition|$publisher/" record_data.txt
+        echo "Data updated successfully"
+        ;;
+
+    7)
+        echo "Exiting the script."
+        exit 0
+        ;;
+
+    *)
+        echo "Enter a valid choice."
+        ;;
     esac
-
-    read -p "Press Enter to continue..."
 done
